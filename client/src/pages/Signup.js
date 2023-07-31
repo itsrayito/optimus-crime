2d7d71b8d6e0c7954d9a25208dfb3caaf2fcd301
itsrayito <rayito.aguirre94@gmail.com>
2023-07-31 13:50:57 -0500

    signup function, & pseudocode

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { ADD_USER } from "../utils/mutations";
 import { useMutation } from "@apollo/react-hooks";
 
-function Signup() {
+function Signup(props) {
     const [formState, setFormState] = useState(
         { email: '', password: ''});
         const [addUser] = useMutation(ADD_USER);
@@ -18,13 +18,69 @@ function Signup() {
                     lastName: formState.lastName
                 },
             });
+            // installation of jwt-decode needed
+            const token = mutationResponse.data.addUser.token;
+        
         };
 
-        const handleChange = event => {
+        const handleChange = (event) => {
             const { name, value } = event.target;
             setFormState({
                 ...formState,
-                [name]: value
+                [name]: value,
             });
         };
+
+        return (
+            <div className="">
+                <h2>Signup</h2>
+                <form onSubmit={handleFormSubmit}>
+                    <div className="">
+                        <label htmlFor="firstName">First Name:</label>
+                        <input
+                        placeholder="First"
+                        name="firstName"
+                        type="firstName"
+                        id="firstName"
+                        onChange={handleChange}
+                        />
+                    </div>
+                    <div className="">
+                        <label htmlFor="lastName">Last Name:</label>
+                        <input
+                        placeholder="Last"
+                        name="lastName"
+                        type="lastName"
+                        id="lastName"
+                        onChange={handleChange}
+                        />
+                    </div>
+                    <div className="">
+                        <label htmlFor="email">Email:</label>
+                        <input
+                        placeholder="youremail@test.com"
+                        name="email"
+                        type="email"
+                        id="email"
+                        onChange={handleChange}
+                        />
+                    </div>
+                    <div className="">
+                        <label htmlFor="pwd">Password:</label>
+                        <input
+                        placeholder="******"
+                        name="password"
+                        type="password"
+                        id="pwd"
+                        onChange={handleChange}
+                        />
+                    </div>
+                    <div className="">
+                        <button type="submit">Submit</button>
+                    </div>
+                </form>
+            </div>
+        );
 }
+
+export default Signup;
