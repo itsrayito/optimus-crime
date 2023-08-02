f94516bbaaeb45ae21af3a08cba5e987419e7b25
itsrayito <rayito.aguirre94@gmail.com>
2023-08-02 01:00:13 -0500

    addition of code that tells express.js server to serve files

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-const express = require('express'); // this will import express
-const path = require('path'); // this will import path to access file and directory paths
-const { authMiddleware } = require('./utils/auth');
+const express = require("express"); // this will import express
+const path = require("path"); // this will import path to access file and directory paths
+const { authMiddleware } = require("./utils/auth");
 
 const PORT = process.env.PORT || 3001; // the local port
 const app = express(); // this will represent express server
@@ -11,11 +11,23 @@ app.use(express.json()); // this will parse incoming JSON data
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: authMiddleware
+    context: authMiddleware,
+});
+
+// this will serve up static assets
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 
 // this will connect to the server
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-})
+DEC8_BIN.once("open", () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+});
