2dd8468f69d12ffc2460419ed945c4d9fb349142
itsrayito <rayito.aguirre94@gmail.com>
2023-08-02 00:23:38 -0500

    addition of the comment & cases query and implemented in home component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { useQuery } from '@apollo/react-hooks';
+import { QUERY_CASES } from "../utils/queries"
+
 import CaseList from '../components/CaseList';
 
 const Home = () => {
+    const { loading, data } = useQuery(QUERY_CASES);
+    const cases = data?.cases || [];
+
     return (
         <main className="main_wrapper">
             <div>
-                <div>CaseList: <CaseList /></div>
+                <div>
+                    The CaseList: <CaseList />
+                </div>
             </div>
         </main>
-    )
+    );
 };
 
 export default Home;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,25 @@
-import gql from 'graphql-tag';
+import gql from "graphql-tag";
+
+export const QUERY_COMMENTS = gql`
+query comments($username: String) {
+    comments(username: $username) {
+        _id
+        commentText
+        createdAt
+        username
+    }
+}
+`;
+
+export const QUERY_CASES = gql`
+{
+    cases {
+        _id
+        name
+        description
+        category {
+            name
+        }
+    }
+}
+`;
