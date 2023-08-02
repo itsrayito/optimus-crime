2619d640439a8edcd49cd4904e8851faa4d2f449
itsrayito <rayito.aguirre94@gmail.com>
2023-08-01 23:41:38 -0500

    added & imported auth middleware & apolloServer

--- server/server.js
+++ server/server.js
@@ -1,7 +1,21 @@
-const express = require("express"); // this will import express
+const express = require('express'); // this will import express
+const path = require('path'); // this will import path to access file and directory paths
+const { authMiddleware } = require('./utils/auth');
 
-const app = express();
+const PORT = process.env.PORT || 3001; // the local port
+const app = express(); // this will represent express server
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.use(express.urlencoded({ extended: true })); // this will parse incoming string or array data
+app.use(express.json()); // this will parse incoming JSON data 
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+});
+
+
+// this will connect to the server
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 })
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -8,5 +8,34 @@ module.exports = {
         const payload = { username, email, _id };
 
         return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    },
+
+    authMiddleware: function({ req }) {
+        // this will allow token to be sent via req.body, req,query, or headers
+        let token = req.body.token || req.query.token || req.headers.authorization;
+
+        // this separates "Bearer" from "<tokenvalue>"
+        if (req.headers.authorization) {
+            token = token
+            .split(' ')
+            .pop()
+            .trim();
+        }
+
+        // if there is no token, this will return request object as is
+        if (!token) {
+            return req;
+        }
+
+        try {
+            // this will decode and attach user data to request object
+            const { data } = jwt.verify(token, secret, { maxAge: expiration });
+            req.user = data;
+        } catch {
+            console.log('Invalid token');
+        }
+
+        // this will return updated request object
+        return req;
     }
 };
