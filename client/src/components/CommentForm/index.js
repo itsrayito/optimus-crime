4aeed783a2b782ffb48e20a186ba9f87be57de22
itsrayito <rayito.aguirre94@gmail.com>
2023-08-02 02:43:49 -0500

    addition of submit form listener and addition of add comment mutation

--- client/src/components/CommentForm/index.js
+++ client/src/components/CommentForm/index.js
@@ -4,18 +4,29 @@ const CommentForm = () => {
     const [commentText, setText] = useState("");
     const [characterCount, setCharacterCount] = useState(0);
 
+    // event listeners
     const handleChange = (event) => {
         if (event.target.value.length <=500) {
             setText(event.target.value);
             setCharacterCount(event.target.value.length);
         }
     };
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        setText("");
+        setCharacterCount(0);
+    };
+
+
     return (
         <div>
            <p className={`m-0 ${characterCount === 500 ? "text-error" : ""}`}>
             Character Count: {characterCount}/500
             </p>{" "}
-            <form className="flex-row justify-center justify-space-between-md align-stretch">
+            <form
+            className="flex-row justify-center justify-space-between-md align-stretch"
+            onSubmit={handleFormSubmit}
+            >
                 <textarea
                 placeholder="Here is a new comment.."
                 value={commentText}
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,33 +1,10 @@
-import gql from 'graphql-tag';
-
-export const LOGIN = gql`
-mutation login($email: String!, $password: ) {
-    login(email: $email, password: $password) {
-    token
-    user {
+export const ADD_COMMENT = gql`
+mutation addComment($commentText: String!) {
+    addComment(commentText: $commentText) {
         _id
-    }
-  }
-}
-`;
-
-export const ADD_USER = gql`
-mutation addUser($firstName: String!, $$lastName: String!, $email: String!, $password: String!) {
-    addUser(firstName: $firstName, lastName: $lastName, email: $email, password: $password) {
-        token
-        user {
-            _id
-        }
-      }
-   }
-`;
-
-export const ADD_POST = gql`
-mutation {
-    addPost($title: String!, $body: String!) {
-        _id
-        title
-        body
+        commentText
+        createdAt
+        username
     }
 }
 `;
