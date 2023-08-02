a29f97f36b02ef541ab342ab72970eee4ba29ae8
itsrayito <rayito.aguirre94@gmail.com>
2023-08-02 02:17:04 -0500

    updates on character count

--- client/src/components/CommentForm/index.js
+++ client/src/components/CommentForm/index.js
@@ -1,15 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const CommentForm = () => {
+    const [commentText, setText] = useState("");
+    const [characterCount, setCharacterCount] = useState(0);
+
+    const handleChange = event => {
+        if (event.target.value.length <=500) {
+            setText(event.target.value);
+            setCharacterCount(event.target.value.length);
+        }
+    };
     return (
         <div>
-            <p className="m-0">
-                Character Count: 0/280
-            </p>
+           <p className="m-0">Character Count: 0/280</p>
             <form className="flex-row justify-center justify-space-between-md align-stretch">
                 <textarea
                 placeholder="Here is a new comment.."
+                value={commentText}
                 className="form-input col-12 col-md-9"
+                onChange={handleChange}
                 ></textarea>
                 <button className="btn col-12 col-md-3" type="submit">
                     Submit
