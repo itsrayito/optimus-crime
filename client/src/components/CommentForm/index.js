2ef4b1f580609032b794c8f4cd123ee2ecc562c8
itsrayito <rayito.aguirre94@gmail.com>
2023-08-03 12:01:36 -0500

    updates to the mutation hook

--- client/src/components/CommentForm/index.js
+++ client/src/components/CommentForm/index.js
@@ -1,15 +1,74 @@
 import React, { useState } from "react";
+import { useMutation } from '@apollo/react-hooks';
+import { ADD_COMMENT } from '../../utils/mutations';
+import { QUERY_COMMENTS, QUERY_ME } from '../../utils/queries'; 
 
 const CommentForm = () => {
+    const [commentText, setText] = useText("");
+    const [characterCount, setCharacterCount] = useState(0);
+    const [addComment, { error }] = useMutation(ADD_COMMENT);
+
+    const [addComment, { error }] = useMutation(ADD_COMMENT, {
+        update(cache, { data: { addComment } }) {
+            try {
+                // this could probably not exist yet, so wrap in a try...catch
+                const { comments } = cache.readQuery({ query: QUERY_COMMENTS });
+                cache.writeQuery({
+                    query: QUERY_COMMENTS,
+                    data: { comments: [addComment, ...comments] }
+                });
+            } catch (e) {
+                console.error(e);
+            }
+
+            // this will update me object's cache, appending new thought to the end of the array
+            const { me } = cache.readQuery({ query: QUERY_ME });
+            cache.writeQuery({
+                query: QUERY_ME,
+                data: { me: { ...me, thoughts: [...me.thoughts, addThought] } }
+            });
+        }
+    });
+
+    // event listeners
+    const handleChange = (event) => {
+        if (event.target.value.length <= 100000) {
+            setText(event.target.value);
+            setCharacterCount(event.target.value.length);
+        }
+    };
+    const handleFormSubmit = async event => {
+        event.preventDefault();
+
+        try {
+            // this will add comment to the database
+            await addComment({
+                variables: { commentText }
+            });
+
+            // this will clear the form value
+            setText('');
+            setCharacterCount(0);
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
     return (
         <div>
-            <p className="m-0">
-                Character Count: 0/280
-            </p>
-            <form className="flex-row justify-center justify-space-between-md align-stretch">
+            <p className={`m-0 ${characterCount === 100000 ? "text-error" : ""} `}>
+                Character Count: {characterCount}/100000
+                {error && <span className="ml-2">Something went wrong..</span>}
+            </p>{" "}
+            <form
+            className="flex-row justify-center justify-space-between-md align-stretch"
+            onSubmit={handleFormSubmit}
+            >
                 <textarea
                 placeholder="Here is a new comment.."
+                value={commentText}
                 className="form-input col-12 col-md-9"
+                onChange={handleChange}
                 ></textarea>
                 <button className="btn col-12 col-md-3" type="submit">
                     Submit
