61bf5706e5bdedefc99d1b9b78530eaacaeb06e4
itsrayito <rayito.aguirre94@gmail.com>
2023-08-08 00:10:21 -0500

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,9 +2,6 @@
 const mongoose = require('mongoose');
 
 // this is the connection to the database
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/optimus-crime', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/optimus-crime');
 
 module.exports = mongoose.connection;                                                      
